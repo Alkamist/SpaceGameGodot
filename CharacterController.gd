a00e5b305e8fa1d3e9ad8220fff56d1dd3f85c54
Corey Lehman <coreylehman@me.com>
2020-10-29 12:15:33 -0400

    Added a jetpack and different aerial movement.

--- CharacterController.gd
+++ CharacterController.gd
@@ -6,9 +6,14 @@ export(NodePath) var gravity_list_path
 export(NodePath) var gravity_tween_path
 export(NodePath) var head_path
 export(float) var mouse_sensitivity = 1.0
-export(float) var movement_speed := 50.0
-export(float) var movement_acceleration := 1000.0
-export(float) var movement_traction := 1000.0
+export(float) var ground_speed := 50.0
+export(float) var ground_acceleration := 1000.0
+export(float) var ground_traction := 1000.0
+export(float) var air_speed := 25.0
+export(float) var air_acceleration := 80.0
+export(float) var air_traction := 80.0
+export(float) var jetpack_speed := 100.0
+export(float) var jetpack_acceleration := 200.0
 export(float) var jump_strength := 50.0
 
 onready var gravity_list: GravityList = get_node(gravity_list_path)
@@ -18,6 +23,8 @@ onready var head: Spatial = get_node(head_path)
 var is_on_ground := false
 var closest_gravity_source: GravitySource
 var bound_gravity_source: GravitySource
+var gravity_normal: Vector3
+var up_normal: Vector3
 var forward_movement := 0.0
 var strafe_movement := 0.0
 var gravity_rotation_weight := 1.0
@@ -25,10 +32,11 @@ var max_move_recursions := 4
 var move_recursions := 0
 
 
-func velocity_movement_addition(target: Vector3, weight: float) -> Vector3:
-    var movement_plane: Plane = Plane(orientation.transform.basis.y.normalized(), 0.0)
+func horizontal_velocity_movement_addition(target: Vector3, weight: float) -> Vector3:
+    var movement_plane: Plane = Plane(up_normal, 0.0)
+    var target_in_movement_plane: Vector3 = movement_plane.project(target)
     var velocity_in_movement_plane: Vector3 = movement_plane.project(velocity)
-    return velocity_in_movement_plane.move_toward(target, weight) - velocity_in_movement_plane
+    return velocity_in_movement_plane.move_toward(target_in_movement_plane, weight) - velocity_in_movement_plane
 
 
 func initiate_orientation_to_new_gravity_source() -> void:
@@ -66,11 +74,11 @@ func update_gravity_source() -> void:
         bound_gravity_source = null
 
 
-func handle_gravity_rotation(gravity_normal: Vector3) -> void:
+func handle_gravity_rotation() -> void:
     var orientation_basis: Basis = orientation.transform.basis
-    var angle_to_up: float = orientation_basis.y.angle_to(-gravity_normal)
+    var angle_to_up: float = orientation_basis.y.angle_to(up_normal)
     if angle_to_up > 0.0:
-        var rotation_axis: Vector3 = orientation_basis.y.cross(-gravity_normal).normalized()
+        var rotation_axis: Vector3 = orientation_basis.y.cross(up_normal).normalized()
         var target_transform: Transform = orientation.transform.rotated(rotation_axis, angle_to_up)
         orientation.transform = orientation.transform.interpolate_with(target_transform, gravity_rotation_weight)
 
@@ -89,19 +97,33 @@ func handle_mouse_looking(mouse_change: Vector2) -> void:
         head.rotation_degrees = rotation
 
 
-func handle_horizontal_movement(delta: float) -> void:
+func handle_grounded_horizontal_movement(delta: float) -> void:
     if forward_movement != 0.0 or strafe_movement != 0.0:
         var forward: Vector3 = -orientation.transform.basis.z.normalized()
         var sideways: Vector3 = orientation.transform.basis.x.normalized()
         var movement_direction: Vector3 = (forward * forward_movement + sideways * strafe_movement).normalized()
-        velocity += velocity_movement_addition(movement_direction * movement_speed, movement_acceleration * delta)
+        velocity += horizontal_velocity_movement_addition(movement_direction * ground_speed, ground_acceleration * delta)
     else:
-        velocity += velocity_movement_addition(Vector3.ZERO, movement_traction * delta)
+        velocity += horizontal_velocity_movement_addition(Vector3.ZERO, ground_traction * delta)
+
+
+func handle_aerial_horizontal_movement(delta: float) -> void:
+    if forward_movement != 0.0 or strafe_movement != 0.0:
+        var forward: Vector3 = -orientation.transform.basis.z.normalized()
+        var sideways: Vector3 = orientation.transform.basis.x.normalized()
+        var movement_direction: Vector3 = (forward * forward_movement + sideways * strafe_movement).normalized()
+        velocity += horizontal_velocity_movement_addition(movement_direction * air_speed, air_acceleration * delta)
+    else:
+        velocity += horizontal_velocity_movement_addition(Vector3.ZERO, air_traction * delta)
+
+
+func handle_jetpack_movement(delta: float) -> void:
+    var upward_velocity: Vector3 = up_normal * velocity.dot(up_normal)
+    velocity += upward_velocity.move_toward(up_normal * jetpack_speed, jetpack_acceleration * delta) - upward_velocity
 
 
 func check_if_on_ground(tolerance_distance: float) -> void:
-    var down_normal: Vector3 = vector_to_gravity_source(bound_gravity_source).normalized()
-    var collision: KinematicCollision = move_and_collide(down_normal * tolerance_distance, true, true, true)
+    var collision: KinematicCollision = move_and_collide(gravity_normal * tolerance_distance, true, true, true)
     is_on_ground = collision != null
 
 
@@ -143,16 +165,23 @@ func _physics_process(delta: float) -> void:
         update_gravity_source()
 
     if bound_gravity_source:
-        var gravity_normal: Vector3 = vector_to_gravity_source(bound_gravity_source).normalized()
-        handle_gravity_rotation(gravity_normal)
+        gravity_normal = vector_to_gravity_source(bound_gravity_source).normalized()
+        up_normal = -gravity_normal
+        handle_gravity_rotation()
 
     check_if_on_ground(0.2)
 
     if is_on_ground:
         if Input.is_action_just_pressed("jump"):
-            velocity += -vector_to_gravity_source(bound_gravity_source).normalized() * jump_strength
+            velocity += up_normal * jump_strength
 
-    handle_horizontal_movement(delta)
+        handle_grounded_horizontal_movement(delta)
+
+    else:
+        if Input.is_action_pressed("jump"):
+            handle_jetpack_movement(delta)
+
+        handle_aerial_horizontal_movement(delta)
 
     move(velocity * delta)
 
